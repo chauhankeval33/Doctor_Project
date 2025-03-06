00d6b08affedf649f71527184dcbe2a5ba40358c
keval chauhan <kevalchauhan1419@gmail.com>
2025-03-06 22:28:41 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = "₹"
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://doctor-project-backend-e8xd.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
