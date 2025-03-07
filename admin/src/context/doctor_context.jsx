cfaeb31e34603d06c599da4c1633829ecc19a347
chauhankeval33 <kevalchauhan1419@gmail.com>
2025-03-07 21:34:36 +0530

    changes:

--- admin/src/context/doctor_context.jsx
+++ admin/src/context/doctor_context.jsx
@@ -7,7 +7,7 @@ export const doctor_context = createContext()
 
 const DoctorContextProvider = (props) => {
 
-    const backendUrl = "https://doctor-project-backend-e8xd.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [dToken, setDToken] = useState(localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "")
     const [appointment, setAppointment] = useState([])
